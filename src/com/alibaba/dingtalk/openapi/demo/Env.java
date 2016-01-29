2a321a2c09b8d70b34757f967f7d886eef6cf77e
ddtalk <ddtalk@users.noreply.github.com>
2016-01-29 13:39:24 +0800

    Update Env.java

--- src/com/alibaba/dingtalk/openapi/demo/Env.java
+++ src/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -17,9 +17,9 @@ public class Env {
 	public static String suiteToken; 
 
 	public static final String CREATE_SUITE_KEY = "suite4xxxxxxxxxxxxxxx";
-	public static final String SUITE_KEY = "suite08kvjy1zvjhuovz3";
-	public static final String SUITE_SECRET = "CRrJy7ikPT9ID9yVnQBLIejIjt0g9dJYfSEssmQJDeXiSFRQbOeJe5aOfcP06l2A";
-	public static final String TOKEN = "123";
-	public static final String ENCODING_AES_KEY = "4371p1pb57uxpq4zuel0s89upw2cipufd0gimy3hi5g";
+	public static final String SUITE_KEY = "";
+	public static final String SUITE_SECRET = "";
+	public static final String TOKEN = "";
+	public static final String ENCODING_AES_KEY = "";
 	
 }
